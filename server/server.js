369ba91d0eb23f54236c590b0ef5413c7fc35f9f
Amr Galal <amrheshamwahba@aucegypt.edu>
2019-04-08 23:31:37 +0200

    Greet new users and alert others

--- server/server.js
+++ server/server.js
@@ -12,9 +12,22 @@ var io = socketIo(server); //here we have our web socket server
 //register an event listener by io.on
 io.on("connection",(socket)=>{ //this listens to the connection event(when a client connects to the server)
   console.log('New user arrived!');
+
+  socket.emit('newMessage',{
+    from: "Admin",
+    text: "Welcome to the chat app",
+    createdAt: new Date().getTime()
+  })
+
+  socket.broadcast.emit('newMessage',{
+    from: "Admin",
+    text: "New user joined the chatroom",
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage',(message)=>{
     console.log('Created a new message ', message);
-    io.emit('newMessage',{
+    socket.broadcast.emit('newMessage',{
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
@@ -22,6 +35,11 @@ io.on("connection",(socket)=>{ //this listens to the connection event(when a cli
   })
   socket.on('disconnect', (reason) => {
     console.log('User disconnected because ' + reason);
+    socket.broadcast.emit('newMessage',{
+      from: "admin",
+      text: "New user left the chatroom",
+      createdAt: new Date().getTime()
+    })
   });
 })
 const port = process.env.PORT || 3000;
